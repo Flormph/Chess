d7e3958228bc3739743f86b1333778eb75d02649
Flormph <josephlee11111010000@gmail.com>
2024-02-26 15:44:30 -0700

    Updated starter code to the current code which will now properly create a file path
    TODO provide an endpoint so the server will initialize

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,7 +2,8 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess server.Server: " + piece);
+        server.Server server = new server.Server();
+        int result = server.run(8080);
+
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,15 +2,12 @@ package server;
 
 import spark.*;
 
-import java.nio.file.Paths;
-
 public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
-        var webDir = Paths.get(Server.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "web");
-        Spark.externalStaticFileLocation(webDir.toString());
+        Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
 
@@ -20,5 +17,6 @@ public class Server {
 
     public void stop() {
         Spark.stop();
+        Spark.awaitStop();
     }
 }

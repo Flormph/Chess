92a0e72a1f3dda08f3571c712e38dd9a1ab92ac7
Flormph <josephlee11111010000@gmail.com>
2024-04-10 15:38:07 -0600

    Added quit

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -179,8 +179,37 @@ public class Main {
         }
     }
 
-    private static void quit() {
-        // logout api here if logged in
+    private static void quit() throws Exception {
+        URI uri = new URI("http://localhost:8080/session");
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod("DELETE");
+
+        http.setDoOutput(true);
+
+        http.connect();
+
+        if(http.getResponseCode() == 200) {
+            System.out.println("Logged out successfully!");
+            setToken(null);
+            isLoggedIn = false;
+        }
+        else {
+            String responseBody = null;
+            System.out.println("Failed to logout");
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+                responseBody = new Gson().fromJson(inputStreamReader, Map.class).toString();
+                System.out.println("Error: " + http.getResponseCode() + " " + responseBody);
+            }
+            System.out.println("quit anyway? (y/n)");
+            if(new Scanner(System.in).next().equalsIgnoreCase("n")) {
+                return;
+            }
+            else {
+                System.exit(0);
+            }
+        }
+        setToken(null);
         System.exit(0);
     }
 
